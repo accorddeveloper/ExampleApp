9fb60ca2d524bf6375155d071b5187b0cc2428bf
Stephen Moss <bladexr8@gmail.com>
2016-06-20 19:57:59 +1000

    Initial Executable API and Application

---
+++ ExampleApp/scripts/exampleapp.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    console.log("***Getting Products...");
+    getProducts = function () {
+        $.ajax("/api/products", {
+            success: function (data) {
+                products.removeAll();
+                for (var i = 0; i < data.length; i++) {
+                    products.push(data[i]);
+                }
+                console.log("***Received Products...");
+                console.log(products);
+            }
+        })
+    };
+    ko.applyBindings();
+});
